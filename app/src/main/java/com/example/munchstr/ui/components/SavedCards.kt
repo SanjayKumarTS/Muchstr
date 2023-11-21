42c4c42649ff2c88daec827fbbcd7837a5e4d23a
sujay malghan <sujayvmalghan@gmail.com>
2023-11-20 21:03:08 -0500

    added saved view

---
+++ app/src/main/java/com/example/munchstr/ui/components/SavedCards.kt
@@ -0,0 +1,107 @@
+package com.example.munchstr.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import com.example.munchstr.R
+import com.example.munchstr.model.Recipe
+
+@Composable
+ fun SavedCards(recipe: Recipe, onClick: () -> Unit){
+    ElevatedCard(onClick = onClick , modifier = Modifier
+        .fillMaxWidth()
+        .padding(15.dp)
+    )
+    {
+        Box(modifier = Modifier.fillMaxWidth()) {
+            UserIconAndName()
+            IconButton(onClick = { },
+                modifier = Modifier.align(Alignment.TopEnd)) {
+                Icon(Icons.Filled.Delete, contentDescription = "Delete")
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .height(200.dp)
+                .fillMaxWidth()
+        ) {
+            AppGlideSubcomposition(imageUri = recipe.photo)
+        }
+        Column(modifier = Modifier.padding(horizontal = 20.dp)){
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Text(text = recipe.name, style = MaterialTheme.typography
+                    .titleLarge,
+                    color = MaterialTheme.colorScheme.onSecondaryContainer,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+                LikeButton(tint = Color(28,27,31))
+            }
+            Text(
+                text = recipe.description,
+                style = MaterialTheme.typography.bodyMedium,
+                maxLines = 3,
+                overflow = TextOverflow.Ellipsis,
+                modifier = Modifier.padding(bottom = 10.dp),
+                color = Color(147,142,142)
+            )
+            Row(verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 20.dp)) {
+                Row (verticalAlignment = Alignment.CenterVertically){
+                    Text(
+                        text = "32 Likes",
+                        style = MaterialTheme.typography.labelSmall,
+                        color = MaterialTheme.colorScheme.onSecondaryContainer
+                    )
+                    Image(painter = painterResource(id = R.drawable
+                        .check_indeterminate_small) ,
+                        contentDescription = "")
+                    Text(
+                        text = "12 Comments",
+                        style = MaterialTheme.typography.labelSmall,
+                        color = MaterialTheme.colorScheme.onSecondaryContainer
+                    )
+                }
+                Button(onClick = { onClick }, colors = ButtonDefaults
+                    .buttonColors(
+                        containerColor = MaterialTheme.colorScheme.primary,
+                        contentColor = MaterialTheme.colorScheme.onPrimary,
+                    )
+                ) {
+                    Text(text = "View Recipe")
+                }
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/munchstr/ui/screens/bookmarks/Screen.kt
@@ -0,0 +1,100 @@
+package com.example.munchstr.ui.screens.bookmarks
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.times
+import androidx.navigation.NavHostController
+import com.example.munchstr.R
+import com.example.munchstr.model.Recipe
+import com.example.munchstr.ui.components.AppCard
+import com.example.munchstr.ui.screens.home.handleCardClick
+import com.example.munchstr.viewModel.RecipeViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Bookmarks(navController: NavHostController, recipeViewModel: RecipeViewModel) {
+
+    LaunchedEffect(Unit){
+        recipeViewModel.loadRecipes()
+    }
+    val recipes = recipeViewModel.recipes
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    BoxWithConstraints(modifier = Modifier.fillMaxWidth(),
+                    ) {
+                        val dynamicSize =
+                            with(LocalDensity.current) {   if (constraints.maxWidth.toDp() > 600.dp) {
+                                0.05f * constraints.maxWidth.toDp()
+                            } else {
+                                0.1f * constraints.maxWidth.toDp()
+                            }}
+
+                        Row(horizontalArrangement = Arrangement.Start) {
+                            Image(
+                                painter = painterResource(
+                                    id = R.drawable
+                                        .bookmark
+                                ),
+                                contentDescription = "",
+                                modifier = Modifier.size(dynamicSize)
+                            )
+                            Spacer(modifier = Modifier.width(10.dp))
+                            Text(
+                                text = "Saved"
+                            )
+
+                        }
+                    }
+                },
+                modifier = Modifier
+                    .background
+                        (MaterialTheme.colorScheme.surface)
+                    .shadow(
+                        elevation = 5.dp
+                    ),
+            )
+        },
+        containerColor = MaterialTheme.colorScheme.background
+    ) {
+        Column(modifier = Modifier.padding(it)) {
+            LazyColumn(
+                content = {
+                    items(
+                        items = recipes,
+                        key = {item: Recipe ->  item.uuid}
+                    ){recipe ->
+                        AppCard(recipe = recipe, onClick = {
+                            handleCardClick(navController, recipe.uuid)
+                        })
+                    }
+                }
+            )
+        }
+    }
+
+}
